c06f06c4dbd7e4ef20949a57f0f4e827bd70ce57
Felix Lunzenfichter <felixlu@student.ethz.ch>
2020-09-16 22:49:28 +0200

    First commit. Voice Message widget in chat.

---
+++ Voices IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voices IOS
+//
+//  Created by Felix Lunzenfichter on 15.09.20.
+//  Copyright © 2020 Felix Lunzenfichter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
